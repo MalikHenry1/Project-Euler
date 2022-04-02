d3d88740334771e167f3146e67ebcbdd5ba76309
Malik Henry <mhenry30658@gmail.com>
2022-04-02 13:38:10 -0400

    deleted  duplicates

---
+++ euler2.js
@@ -0,0 +1,13 @@
+var S = 0;
+var F = [0,1];
+
+for(var i=2; i < 4000000; i++){
+    F[i] = F[i-1] + F[i-2];
+}
+
+for(i=0; F[i] < 4000000; i++){
+    if(i%3==0){
+    S += F[i];
+    }
+}
+ console.log(S);
