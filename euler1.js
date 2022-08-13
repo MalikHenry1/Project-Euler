ef637771f55908649d0779ba062197c2e2c6c689
Malik Henry <mhenry30658@gmail.com>
2022-08-12 23:14:19 -0400

    cleaned code and comments

--- euler1.js
+++ euler1.js
@@ -1,6 +1,6 @@
 var S = 0;
 
-for (var i=0; i<1000; i++){
+for (var i=0; i<1000; i++){ 
     if(i%3 == 0 || i%5 == 0){
         S += i;
     }
--- euler3.js
+++ euler3.js
@@ -1,22 +1,3 @@
-// var maxPrime = 3;
-// var Factors = [0];
-// var N = 600;
-
-
-
-// for(var i = 1; i<=N; i++){
-//     if(N%i==0){
-//         Factors[i] = i;
-//     }
-// }
-
-// for(var i=1; i<10; i++){
-//     for(var j=1; j<i; j++){
-//         if(i%j==0){
-//             maxPrime = j;
-//         }
-//     }
-// }
 
  // TO-DO: write code for a function that pushes the prime factors into an array, finds the max of that array, and prints the max
 function primeFactors(n)
@@ -28,8 +9,11 @@ function primeFactors(n)
         n = Math.floor(n/2);
     }
  
-    // n must be odd at this point. So we can skip
+    // n must be odd at this point. So we can skip to the next odd number, hence i = i + 2
     // one element (Note i = i +2)
+    // why the square root of n? Consider the factors of 36 (1,36), (2,18), (3,12), (4,9), (6,6)
+    // you can think of the square root as the reflection point of the list of factors after which the previous factors repeat.
+    // Thus, it suffices to set the upper bound of the for loop as the square root of n, and we know the list is exhasutive.
     for (let i = 3; i <= Math.sqrt(n); i = i + 2)
     {
         // While i divides n, print i and divide n
@@ -47,7 +31,7 @@ function primeFactors(n)
 }
 
 primeFactors(600851475143);
- 
+
 
 
 
